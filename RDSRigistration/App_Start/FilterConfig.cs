890843842c5a0813e2c609012fd2912979c9754e
SADDAM ALSALEM <sa-alsalem@riyadhschools.edu.sa>
2017-08-22 11:07:20 +0300

    Add project files.

---
+++ RDSRigistration/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RDSRigistration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
